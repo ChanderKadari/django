fa51a79331e62adc3a827e7ec91678491deedb45
chander kadari <kadarichander@gmail.com>
2025-12-02 13:45:03 +0530

    firstdeploy

---
+++ web/view.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render
+def about(request):
+    return render(request,'about.html')
+def home(request):
+    return render(request,'home.html')
+def contact(request):
+    return render (request,'contact.html')
+def login (request):
+    return render(request,'login.html')
+def register(request):
+    return render(request,'register.html')
